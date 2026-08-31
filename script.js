2bf6ad3ff8c8a34d8b8895adf6cd643e3970854e
HamnaTanveer <hamnat992@gmail.com>
2026-08-31 21:08:47 +0500

    Remove extra close button from script.js

--- script.js
+++ script.js
@@ -2,17 +2,12 @@ const foodContainer = document.getElementsByClassName("foodContainer")[0];
 const searchBox = document.getElementById('search-input');
 const mainContainer = document.getElementsByClassName('search-area')[0];
 
-
 const modalBox = document.querySelector('.modalBox');
 const modalImg = document.getElementById('modalImg');
 const modalTitle = document.getElementById('modalTitle');
 const modalCountry = document.getElementById('modalCountry');
 const modalInstructions = document.getElementById('modalInstructions');
 const modalIngredients = document.getElementById('modalIngredients');
-
-
-
- 
 
 function fetchData() {
   const term = searchBox.value;
@@ -21,76 +16,72 @@ function fetchData() {
     .then(res => res.json())
     .then(data => {
       allMeals = data.meals; 
-      displayData(allMeals);
-      console.log(data.meals)
+      if (allMeals) {
+        displayData(allMeals);
+      } else {
+        foodContainer.innerHTML = "<p style='text-align:center; width:100%;'>No recipes found!</p>";
+      }
     });
 }
 
-// diplayy fnction
+// display function
 function displayData(data) {
-
   foodContainer.innerHTML = "";
 
   data.map((obj) => {
-
     let foodDiv = document.createElement("div");
     foodDiv.className = "foodDiv";
 
     let foodImg = document.createElement("img");
     foodImg.src = obj.strMealThumb;
-    foodImg.className='foodImg';
+    foodImg.className = 'foodImg';
 
     let foodTitle = document.createElement("h2");
     foodTitle.innerText = obj.strMeal;
     foodTitle.className = 'foodTitle';
 
-     let foodDesc = document.createElement("h2");
+    let foodDesc = document.createElement("h2");
     foodDesc.innerText = obj.strArea + " " + "Recipe";
     foodDesc.className = 'foodDesc';
 
-    let foodSubTitle= document.createElement('p')
-    foodSubTitle.className='foodSubTitle';
-    foodSubTitle.innerText= 'Please Click Here to see Details...';
-
+    let foodSubTitle = document.createElement('p');
+    foodSubTitle.className = 'foodSubTitle';
+    foodSubTitle.innerText = 'Please Click Here to see Details...';
 
     let clickBtn = document.createElement('button');
     clickBtn.className = 'clickBtn';
     clickBtn.innerText = 'Click Here';
 
+    // modal function
+    function showModel(obj) {
+      foodContainer.style.filter = 'blur(2px)';
+      modalBox.style.display = 'flex';
 
-    //model fnction
+      // data set 
+      modalImg.src = obj.strMealThumb;
+      modalTitle.innerText = obj.strMeal;
+      modalCountry.innerText = obj.strArea;
+      modalInstructions.innerText = obj.strInstructions;
 
-    function showModel(obj){
-    foodContainer.style.filter = 'blur(2px)';
-    modalBox.style.display = 'flex';
+      modalIngredients.innerHTML = `
+        <li>${obj.strIngredient1 || ''}</li>
+        <li>${obj.strIngredient2 || ''}</li>
+        <li>${obj.strIngredient3 || ''}</li>
+        <li>${obj.strIngredient4 || ''}</li>
+        <li>${obj.strIngredient5 || ''}</li>
+        <li>${obj.strIngredient6 || ''}</li>
+        <li>${obj.strIngredient7 || ''}</li>
+        <li>${obj.strIngredient8 || ''}</li>
+        <li>${obj.strIngredient9 || ''}</li>
+        <li>${obj.strIngredient10 || ''}</li>
+      `; 
+    }
 
-  // data set 
-  modalImg.src = obj.strMealThumb;
-  modalTitle.innerText = obj.strMeal;
-  modalCountry.innerText = obj.strArea;
-  modalInstructions.innerText = obj.strInstructions;
-
-    modalIngredients.innerHTML = `
-    <li>${obj.strIngredient1}</li>
-    <li>${obj.strIngredient2}</li>
-    <li>${obj.strIngredient3}</li>
-    <li>${obj.strIngredient4}</li>
-    <li>${obj.strIngredient5}</li>
-    <li>${obj.strIngredient6}</li>
-    <li>${obj.strIngredient7}</li>
-    <li>${obj.strIngredient8}</li>
-    <li>${obj.strIngredient9}</li>
-    <li>${obj.strIngredient10}</li>
-    
-  `; 
-   
-  };
     clickBtn.onclick = () => {
-    showModel(obj);
-      };
+      showModel(obj);
+    };
 
-
-    //append childdd
+    // append child
     foodDiv.appendChild(foodImg);
     foodDiv.appendChild(foodTitle);
     foodDiv.appendChild(foodDesc);
@@ -98,19 +89,13 @@ function displayData(data) {
     foodDiv.appendChild(clickBtn);
 
     foodContainer.appendChild(foodDiv);
-
   });
 }
-//close btn
-let closeBtn = document.createElement('i');
-closeBtn.className = "far fa-window-close closeBtn";
 
-modalBox.appendChild(closeBtn);
-
-// function close btn
-closeBtn.onclick = () => {
+// Close Modal Function for HTML click
+function closeModal() {
   modalBox.style.display = 'none';
   foodContainer.style.filter = 'blur(0)';
-};
+}
 
 fetchData();
